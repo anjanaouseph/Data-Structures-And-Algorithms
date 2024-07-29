0873b47e70cb72826740565efdb749cc5faf9b21
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2024-07-30 00:37:13 +0530

    Time: 30 ms (87.97%), Space: 16.6 MB (41.86%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        map = {')':'(', '}':'{', ']':'['}
+
+        for c in s:
+            if c not in map:
+                stack.append(c) #opening brackets append to stack
+                continue
+            if not stack or stack[-1] != map[c]:
+                return False
+            stack.pop()
+        return not stack
+
+
+        
