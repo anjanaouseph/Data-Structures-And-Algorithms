bb77db491ad528bc0e5191356a0ae236175c34a6
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2024-09-14 13:40:51 +0530

    Time: 666 ms (62.7%), Space: 33.6 MB (20.26%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
@@ -5,14 +5,13 @@ class Solution:
 
         for i,h in enumerate(heights):
             start = i
-            while stack and stack[-1][1] > h: #if the second block is smaller than first one then we can't extend it's area to the right
-                index,height = stack.pop() #this index will be the starting of its area count
-                max_area = max(max_area, height*(i-index)) #area will be (current loop position - the start of area) * height
-                start = index #the smaller block can extend left 
-            stack.append((start, h))
-
-            #find the area of the remaning smaller blocks
-        for index,height in stack:
-            max_area =  max(max_area, height*(len(heights)-index))
+            while stack and stack[-1][1] > h:
+                index, height = stack.pop()
+                start = index
+                max_area = max(max_area, (height * (i - index)))
+            stack.append((start,h))
+        
+        for i,h in stack:
+            max_area = max(max_area, h * (len(heights) - i))
 
         return max_area
