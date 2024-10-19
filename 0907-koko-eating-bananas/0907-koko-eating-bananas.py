84ca515da5517bc10bc251e765eef1f5d29149cf
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2024-10-19 18:30:43 -0400

    Time: 145 ms (99.18%), Space: 17.8 MB (98.58%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.py
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.py
@@ -13,7 +13,7 @@ class Solution:
                 left = mid+1
                 #dont update res when it takes more than h hours, because say mid is 3, it will take 10 hours, then res will be set as 3 even if its the incorrect ans (correct ans is 4 hrs.)
             else:
-                #even if k=h, we should search for lower count value else you will get some lower no which may be the wrong answer
+                #even if k=h, we should search for lower count value else you will get some lower no which may be the wrong answer like say 6 but (even though it takes only 7 hrs) still 4 is the optimal ans even though it takes 8 hrs 
                 res = min(mid,res) 
                 right = mid-1
 #The variable res should be updated only when a feasible solution (k <= h) is found. You are doing this correctly in your description. However, in practice, if k == h, you might still want to try finding a lower mid if possible.
