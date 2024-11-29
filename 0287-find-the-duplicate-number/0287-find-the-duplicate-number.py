15ec5731cbe3546b8257f8321410cfb300db554d
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2024-11-28 21:13:27 -0700

    Time: 34 ms (34.78%), Space: 29 MB (48.83%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -1,20 +1,19 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
 
-        slow = nums[0]
-        fast = nums[0]
+        slow, fast = 0,0
+
         while True:
             slow = nums[slow]
             fast = nums[nums[fast]]
             if slow == fast:
                 break
 
-        slow2 = nums[0]
+        slow2 = 0
 
-        while slow != slow2:#check if they are equal initally itself
-        #because if start of the cycle itself is at nums[0] then we need to return that as duplicate not the next nums[slow] or nums[slow2]
-            slow = nums[slow]   # Move both pointers by 1 step
+        while True:
+            slow = nums[slow]
             slow2 = nums[slow2]
-
-        return slow
+            if slow == slow2:
+                return slow
         
