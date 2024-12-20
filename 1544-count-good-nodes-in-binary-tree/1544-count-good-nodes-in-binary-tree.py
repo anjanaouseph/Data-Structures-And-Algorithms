724458e015800527a9d94c0124b5501d1f30c7d9
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2024-12-19 19:13:16 -0700

    Time: 137 ms (45.69%), Space: 32.1 MB (5.49%) - LeetHub

--- 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.py
+++ 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.py
@@ -6,21 +6,20 @@
 #         self.right = right
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
+        count = [0]
         #using recurrsion
         def find_nodes(node, value):
             if not node:
                 return 0
 
-            count = 0
-
             if node.val >= value:
                 value = max(node.val, value)
-                count = 1
+                count[0] += 1
 
-            count += find_nodes(node.left, value)
-            count += find_nodes(node.right, value)
+            find_nodes(node.left, value)
+            find_nodes(node.right, value)
 
-            return count
+            return count[0]
 
         return find_nodes(root, root.val)
 
