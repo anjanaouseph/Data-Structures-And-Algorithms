9661ed4dfa63d9fda1ebc0c16af45de3c3db3527
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2024-12-21 22:48:42 -0700

    Time: 4 ms (80.31%), Space: 19.4 MB (65.22%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -6,16 +6,28 @@
 #         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        if not preorder or not inorder:
-            return None
+        #create a hashmap for inorder for lookup
+        hashMap = {val : index for index,val in enumerate(inorder)}
 
-        root = TreeNode(preorder[0]) #preoder[0] element is always gonna be the root node
+        def construct(pre_start, pre_end, in_start, in_end):
+            if pre_start > pre_end or in_start > in_end:
+                return None
 
-        mid = inorder.index(preorder[0])
+            rootval = preorder[pre_start]
 
-        root.left = self.buildTree(preorder[1:mid+1], inorder[:mid])
+            root = TreeNode(rootval)
 
-        root.right = self.buildTree(preorder[mid+1 : ], inorder[mid+1 : ])
+            mid = hashMap[rootval] #use hashmap for O(1) lookup instead of index()
 
-        return root
+            left_elements = mid - in_start
+
+            root.left = construct(pre_start+1, pre_start+left_elements, in_start, mid-1 )#instead of array slicing we pass the pointers
+
+            root.right = construct(pre_start+left_elements+1, pre_end, mid+1, in_end)
+
+            return root
+
+            
+
+        return construct(0, len(preorder)-1, 0, len(inorder)-1)#we pass the indices of elements in each list
         
