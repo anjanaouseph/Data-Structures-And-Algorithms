3fa5c3ea31b0f99567145b2f6b0950f6a1de2810
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2025-01-02 22:42:00 -0700

    Time: 19 ms (55.51%), Space: 18 MB (9.23%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -3,17 +3,19 @@ class Solution:
         l = 0
         r = 0
         max_len = 0
-        map = {}
+        sett = set()
 
-        while r < len(s):
+        while r<len(s):
+            while s[r] in sett:
+                sett.remove(s[l])
+                l = l+1
 
-            if s[r] in map and map[s[r]]>=l: #within the range
-                l = map[s[r]]+1 #jump l to the position after r to get bigger or same size substring
-           
+            sett.add(s[r])
+
             max_len = max(max_len, r-l+1)
 
-            map[s[r]] = r
+            r = r+1
 
-            r += 1
+        return max_len
 
-        return max_len        
+        
