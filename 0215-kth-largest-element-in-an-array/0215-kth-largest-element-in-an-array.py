81dffa9ebdb8282c009423c25fcd801d8eeb73e3
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2025-01-05 14:55:21 -0700

    Time: 140 ms (10.64%), Space: 28.6 MB (29.2%) - LeetHub

---
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+
+        hashMap = {}
+        min_key = max_key =0
+
+        for i in range(len(nums)):
+            hashMap[nums[i]] = hashMap.setdefault(nums[i],0)+1
+            min_key = min(min_key, nums[i])
+            max_key = max(max_key, nums[i])
+        sum = 0
+        for key in range(max_key, min_key - 1, -1):#reverse loop
+            sum += hashMap.get(key, 0)#if key exists else return 0
+            if sum >= k:
+                return key        
