c466a2c7a62e4f47d3f285c66fbe0aa5e33f3217
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2025-01-05 14:58:27 -0700

    Time: 132 ms (10.87%), Space: 28.5 MB (35.85%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -8,8 +8,11 @@ class Solution:
             hashMap[nums[i]] = hashMap.setdefault(nums[i],0)+1
             min_key = min(min_key, nums[i])
             max_key = max(max_key, nums[i])
+
         sum = 0
+        
         for key in range(max_key, min_key - 1, -1):#reverse loop
             sum += hashMap.get(key, 0)#if key exists else return 0
             if sum >= k:
-                return key        
+                return key
+        return 0     
