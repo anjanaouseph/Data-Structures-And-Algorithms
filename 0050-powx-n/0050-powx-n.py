66aa5cf1a9cc3d17288291f0024e579c6a42e4d7
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2025-01-20 19:40:39 -0700

    Time: 0 ms (100%), Space: 18 MB (5.83%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.py
@@ -0,0 +1,15 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        def power(x,n):
+            #base cases
+            if x == 0:
+                return 0
+            if n == 0:
+                return 1
+
+            #now call recursive function
+            res = power(x, n//2)
+            res = res * res if n%2==0 else res * res * x
+            return res
+
+        return power(x,n) if n >=0 else 1/(power(x,-n))
