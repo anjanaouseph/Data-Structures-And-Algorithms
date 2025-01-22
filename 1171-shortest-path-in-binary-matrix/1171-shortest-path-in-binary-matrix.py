c00d443654c047e90fb72f619070ab53409b9a4e
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2025-01-22 13:49:49 -0700

    Time: 247 ms (33.99%), Space: 19.6 MB (17.83%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.py
@@ -1,5 +1,6 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        #DO BFS because we need shortest path
 
         m = len(grid)
         n = len(grid[0])
@@ -15,6 +16,12 @@ class Solution:
 
         distance = 1 #because (0,0) part of the path
 
+        directions = [
+        (-1, -1), (-1, 0), (-1, 1),  # top-left, top, top-right
+        (0, -1),         (0, 1),     # left, right
+        (1, -1), (1, 0), (1, 1)      # bottom-left, bottom, bottom-right
+    ]
+
         def addRow(row,col):
             if row < 0 or row >= m or col<0 or col>=n or (row,col) in visited or grid[row][col] == 1:
                 return
@@ -23,20 +30,15 @@ class Solution:
 
         while queue:
             length = len(queue)
-            for i in range(length):
+            for _ in range(length):
                 r,c = queue.popleft()
 
                 if (r, c) == (m - 1, n - 1):
                     return distance
 
-                addRow(r+1,c+1) #diagonal right bottom
-                addRow(r+1,c-1) #diagonal left bottom
-                addRow(r-1,c-1) #diagonal left top
-                addRow(r-1,c+1) #diagonal right top
-                addRow(r,c+1)
-                addRow(r,c-1)
-                addRow(r-1,c)
-                addRow(r+1,c)
+                for dr, dc in directions:
+                    addRow(r+dr,c+dc)#checks in all 8 directions
+
             distance += 1
                 
         return -1
