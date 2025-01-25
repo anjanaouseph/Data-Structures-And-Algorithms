a2aeb747003463638376b696ea061754227c6c87
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2025-01-25 13:25:20 -0700

    Time: 12 ms (17.61%), Space: 20.9 MB (71.83%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -9,6 +9,6 @@ class Solution:
             if not merged or interval[0] > merged[-1][1]:#the first element is greater than the last element of the merged interval's range then no overlap
                 merged.append(interval)
             else:
-                 merged[-1] = [merged[-1][0],max(merged[-1][1], interval[1])]
+                 merged[-1] = [merged[-1][0],max(merged[-1][1], interval[1])] #avoids needing to pop() and append() like in some other implementations. Instead, it directly modifies the last interval in place, making the code concise and easy to read.
 
         return merged
