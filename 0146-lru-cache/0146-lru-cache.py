0f1ded84f2fe6f5a556a914370e62989804aa0bc
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2025-01-25 21:26:26 -0700

    Time: 171 ms (24.61%), Space: 78.6 MB (23.51%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,51 +1,62 @@
 class Node:
-    def __init__(self,key,val):
+    def __init__(self, key, val):
         self.key = key
         self.val = val
-        self.prev = self.next = None
+        self.next = self.prev = None
 
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.cap = capacity
-        self.cache = {}
+        self.capacity = capacity
 
-        self.left, self.right = Node(0,0),Node(0,0)
+        #hashmap for O(1) get and put else it will be O(n)
+        self.cache = {} #hashmap tp store key and values as Nodes
+
+        #initialize dummy nodes for DLL
+        self.left = self.right = Node(0,0)
+
+        #connect next and prev pointers
         self.left.next = self.right
         self.right.prev = self.left
 
-    def remove(self,node):
-        prev,nxt = node.prev, node.next
+    #remove the node from the LL and add it to the right side
+    def remove(self, node):
+        prev, nxt = node.prev, node.next
         prev.next = nxt
         nxt.prev = prev
 
-    def insert(self,node):
-        prev, nxt = self.right.prev, self.right
+    #insert node to the linkedlist, at the rightmost end as thats the MRU
+    def insert(self, node):
+        prev = self.right.prev
         prev.next = node
-        nxt.prev = node
-        node.next = nxt
+        node.next = self.right
         node.prev = prev
+        self.right.prev = node
+        
 
     def get(self, key: int) -> int:
+        key_val = -1
         if key in self.cache:
-            self.remove(self.cache[key]) #delete the node
-            self.insert(self.cache[key]) #add it to near Most Recently Used
-            return self.cache[key].val
-        return -1
+            self.remove(self.cache[key])#delete the node from DLL
+            self.insert(self.cache[key])#add to right side of the DLL its MRU
+            key_val = self.cache[key].val       
+        return key_val
+        
         
 
     def put(self, key: int, value: int) -> None:
+        node = Node(key, value)
         if key in self.cache:
             self.remove(self.cache[key])
-        self.cache[key] = Node(key,value)
-        self.insert(self.cache[key])
+            self.insert(node)
+            self.cache[key] = node
+        else:
+            self.insert(node)
+            self.cache[key] = node
 
-        if len(self.cache) > self.cap:#if length exceeds capcity evict LRU key
-            lru = self.left.next
-            self.remove(lru)
-            del self.cache[lru.key]#delete key from hashmap
-
-        
+        if len(self.cache) > self.capacity:
+            del self.cache[self.left.next.key]
+            self.remove(self.left.next)
 
 
 # Your LRUCache object will be instantiated and called as such:
