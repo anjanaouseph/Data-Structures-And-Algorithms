368078d314d991324c4aaaf151c957134eb0c00c
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2025-01-26 11:44:03 -0700

    Time: 83 ms (8.52%), Space: 39.5 MB (5.07%) - LeetHub

--- 1909-buildings-with-an-ocean-view/1909-buildings-with-an-ocean-view.py
+++ 1909-buildings-with-an-ocean-view/1909-buildings-with-an-ocean-view.py
@@ -16,6 +16,6 @@ class Solution:
             val, index = stack.pop()
             res.append(index)
 
-        res.reverse()
+        res.reverse() #The reason res.reverse() returns None is because the reverse() method in Python is designed to modify the list in place and not return a new list. This design follows the principle of command-query separation (CQS), which states that a method should either:Perform an action (like modifying an object) but not return a value.Return a value but not modify the object.
         return res
         
