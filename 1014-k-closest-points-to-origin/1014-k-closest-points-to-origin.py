9e28874ae903b68358568143243986dcff4cffe2
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2025-01-27 15:57:26 -0700

    Time: 87 ms (29.91%), Space: 22.5 MB (56.41%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.py
@@ -9,20 +9,18 @@ class Solution:
 
         for point in points:
             distance = math.sqrt((point[0]-0)**2+(point[1]-0)**2)
-            heapq.heappush(heap, (distance, [point[0],point[1]]))
-
-        #now heapify this
-        
-#The heap property is maintained, meaning the smallest element will always be at the root of the heap. However, the heap is not a fully sorted structure. It only guarantees that the smallest element is at the root (the first element in the list), and the remaining elements only satisfy the heap property — not sorted order.
+            heapq.heappush(heap, (-distance, [point[0],point[1]]))
+            if len(heap)>k:#we need a maxheap to pop out the largest elements
+                heapq.heappop(heap) #nlogk
 
         res = []
 
-        for i in range(k):
-            res.append(heapq.heappop(heap)[1]) #so use heappop() to get the sorted order in increasing.
+        for element in heap: 
+            res.append(element[1])
 
         return res
 
-        
-
 
         
+#The heap property is maintained, meaning the smallest element will always be at the root of the heap. However, the heap is not a fully sorted structure. It only guarantees that the smallest element is at the root (the first element in the list), and the remaining elements only satisfy the heap property — not sorted order.
+      
