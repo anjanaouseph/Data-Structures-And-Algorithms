9270b7246dcebe04a302788757b91bc3f31b8e09
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2025-01-29 20:53:08 -0700

    Time: 67 ms (63.81%), Space: 21.5 MB (42.28%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.py
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.py
@@ -2,37 +2,37 @@ class Solution:
     def calculate(self, s: str) -> int:
         if not s:
             return 0
-
+        
         curr_no = 0
-        result = 0
-        last_no = 0
-
         operation = '+'
+        stack = []
 
-        s = s.replace(" ", "")
+        s = s.replace(" ","") #replace all empty spaces
 
         for i,char in enumerate(s):
             if char.isdigit():
                 curr_no = curr_no * 10 + int(char)
 
-            if not char.isdigit() or i == len(s)-1 :
-                if operation == '+' or operation == '-':
-                    result += last_no
-                    last_no = curr_no if operation == '+' else -curr_no
+            if not char.isdigit() or i == len(s)-1:
+                if operation == '+':
+                    stack.append(curr_no)
 
-                if operation == '*':
-                    last_no = last_no * curr_no
+                elif operation == '-':
+                    stack.append(-curr_no)
 
-                if operation == "/":
-                    last_no = int(last_no / curr_no)
+                elif operation == '*':
+                    stack[-1] = stack[-1] * curr_no
+
+                else:
+                    stack[-1] = int(stack[-1] / curr_no)
 
                 operation = char
                 curr_no = 0
-        
-        result += last_no
-                
-        return result
 
-            
-                
-                
+        return sum(stack)
+
+
+
+
+
+        
