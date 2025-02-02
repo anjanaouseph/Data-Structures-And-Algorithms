791572b8d4ef883989ef4fee00e4e1d269eb3188
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2025-02-01 19:07:27 -0700

    Time: 3 ms (86.75%), Space: 20.2 MB (57.43%) - LeetHub

---
+++ 1091-maximum-average-subtree/1091-maximum-average-subtree.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maximumAverageSubtree(self, root: Optional[TreeNode]) -> float:
+        max_val = [0]
+
+        def dfs(root, pos):
+            if not root:
+                return (0,0)
+
+            left, leftpos = dfs(root.left, pos+1)
+            right, rightpos = dfs(root.right, pos+1)
+
+            sum = left+right+root.val
+            count = leftpos+rightpos+1
+
+            average = sum/count #use float
+            max_val[0] = max(average, max_val[0])
+
+            return (sum, count)
+            
+        dfs(root,0)
+        return max_val[0]
