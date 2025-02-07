d59f7c7aa1bb9d55ba914d92bf18bbc2097ccee0
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2025-02-07 13:19:41 -0700

    Time: 1 ms (56.94%), Space: 19.8 MB (11.98%) - LeetHub

---
+++ 0261-graph-valid-tree/0261-graph-valid-tree.py
@@ -0,0 +1,35 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+
+        #empty graph is considered a tree.
+
+        #we make adj list but its bidirectional
+        adj = defaultdict(list)
+        for a,b in edges:
+            adj[a].append(b)
+            adj[b].append(a)
+
+        unvisited, visited, visiting = 0,2,1
+        states = [unvisited]*n
+
+        visit_set = set() 
+
+        def dfs(node, prev):
+            state = states[node]
+            if state == visited: return True
+            if state == visiting: return False
+
+            states[node] = visiting
+
+            for nei in adj[node]:
+                if nei == prev:#skip for false detection of a cycle
+                    continue
+                elif not dfs(nei, node):
+                    return False
+
+            states[node] = visited
+            visit_set.add(node)
+
+            return True
+
+        return dfs(0, -1) and len(visit_set)==n
