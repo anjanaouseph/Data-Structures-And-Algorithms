aa503c4383c052ad2f2b2ed6afa916887dbbf58c
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2025-02-08 20:14:04 -0700

    Time: 1 ms (67.14%), Space: 18.9 MB (5.91%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.py
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -2,11 +2,24 @@ class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
         adj = defaultdict(list)
 
+        n = len(edges)
+        
+        for a, b in edges:
+            adj[a].append(b)
+            adj[b].append(a)
+
         unvisited, visiting = 0, 1
 
-        def dfs(node, prev):   #dfs to check if cycle exists     
+        states = [unvisited] * (n + 1)
 
+        cycle = []
+
+        cycle_start = -1
+
+        def dfs(node, prev):   #dfs to check if cycle exists  
+            nonlocal cycle_start   
             if states[node] == visiting:
+                cycle_start = node #add start of cycle to list
                 return True #cycle detected
 
             states[node] = visiting
@@ -15,15 +28,19 @@ class Solution:
                 if nei == prev:
                     continue
                 elif dfs(nei, node):
+                    if cycle_start != -1:#include only nodes that are part of the cycle.
+                        cycle.append(node)
+                    if cycle_start == node: #we came back to start of cycle again 
+                        cycle_start = -1 #reset cycle start
                     return True
-
+            
             return False
 
-        for a, b in edges:
-            adj[a].append(b)
-            adj[b].append(a)
+        #since the graph that started as a tree from 1 to n, we do dfs for just node 1
 
-            states = [unvisited] * (len(edges) + 1)
-            
-            if dfs(a, -1):
-                return [a,b]
+        dfs(1,-1)
+
+        #check if each vertex in the edges are present in the cycle, that means that edge is the culprit.
+        for i in range(n-1, -1, -1):
+            if edges[i][0] in cycle and edges[i][1] in cycle:
+                return edges[i]
