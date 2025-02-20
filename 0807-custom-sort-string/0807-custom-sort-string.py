b9ed3a610f51e95bc58a245f9fe1fd45f8a2b61a
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2025-02-20 08:21:13 -0700

    Time: 3 ms (14.66%), Space: 17.9 MB (17.16%) - LeetHub

--- 0807-custom-sort-string/0807-custom-sort-string.py
+++ 0807-custom-sort-string/0807-custom-sort-string.py
@@ -1,14 +1,14 @@
 class Solution:
     def customSortString(self, order: str, s: str) -> str:
-        hashMap = {}
+        hashMap = defaultdict(int)
 
         for char in s:
-            hashMap[char] = hashMap.get(char, 0)+1
+            hashMap[char] += 1
 
         string_builder = []
 
         for char in order:
-            if hashMap.get(char, 0):
+            if hashMap[char]:
                 string_builder.append(char * hashMap[char])
                 del hashMap[char] #to remove a key from hashMap if using collections
 
