677e36133ea1b3afb3008dd60ef7a899036a7e2f
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2025-05-01 21:44:26 -0700

    Time: 191 ms (19.06%), Space: 18.9 MB (49.32%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.py
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.py
@@ -1,21 +1,31 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        left,right = 1, max(piles)
-        res = right #it will be atleast the maximum element in the piles array
+         left = 1 #min it can eat 1 banana per hour
+         right = max(piles) #max it can eat highest value in pile
 
-        while left<=right:
+         #conditional binary search
+         while left<right:
             mid = (left+right)//2
-            k = 0 #total hours taken to eat with mid no of bananas
-            for i in piles:
-                k+= math.ceil(i/mid) #find the hours
 
-            if k > h: #takes lots of time so increase the speed (no of bananas)
+            if self.canEat(piles, mid, h):
+                #lets try to find a value lesser
+                right = mid
+            else: #increase the no of bananas per hour
                 left = mid+1
-                #dont update res when it takes more than h hours, because say mid is 3, it will take 10 hours, then res will be set as 3 even if its the incorrect ans (correct ans is 4 hrs.)
-            else:
-                #even if k=h, we should search for lower count value else you will get some lower no which may be the wrong answer like say 6 but (even though it takes only 7 hrs) still 4 is the optimal ans even though it takes 8 hrs 
-                res = min(mid,res) 
-                right = mid-1
-#The variable res should be updated only when a feasible solution (k <= h) is found. You are doing this correctly in your description. However, in practice, if k == h, you might still want to try finding a lower mid if possible.
 
-        return res
+         return left
+
+    def canEat(self,piles, mid, h):
+        count = 0
+        #here find how man hours it takes to finish the whole pile using mid hours
+        for i in range(len(piles)):
+            count += math.ceil(piles[i]/mid)
+
+        if count<=h:
+            return True
+        return False
+
+
+
+
+        
