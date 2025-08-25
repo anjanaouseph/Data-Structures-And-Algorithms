db57acf6cd3beb36f282befaa835bcfed24cb1a4
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2025-08-24 22:50:13 -0700

    Time: 183 ms (23.79%), Space: 18.8 MB (74.99%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.py
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.py
@@ -1,31 +1,24 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-         left = 1 #min it can eat 1 banana per hour
-         right = max(piles) #max it can eat highest value in pile
 
-         #conditional binary search
-         while left<right:
-            mid = (left+right)//2
+        left = 1
+        right = max(piles)
 
-            if self.canEat(piles, mid, h):
-                #lets try to find a value lesser
+        while left<right:
+            mid = left+(right-left)//2
+
+            if self.min_integer(mid, h, piles):
                 right = mid
-            else: #increase the no of bananas per hour
+            else:
                 left = mid+1
+        return left
 
-         return left
 
-    def canEat(self,piles, mid, h):
+    def min_integer(self,k, h, piles):
         count = 0
-        #here find how man hours it takes to finish the whole pile using mid hours
         for i in range(len(piles)):
-            count += math.ceil(piles[i]/mid)
+            count += math.ceil(piles[i]/k)
 
         if count<=h:
             return True
         return False
-
-
-
-
-        
