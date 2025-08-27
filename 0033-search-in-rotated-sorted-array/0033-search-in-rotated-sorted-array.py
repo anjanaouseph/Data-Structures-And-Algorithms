299763a9dfab1e1deb9ae5a20cbf209857bfd41e
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2025-08-27 11:15:18 -0700

    Time: 0 ms (100%), Space: 18.1 MB (66.56%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,25 +1,41 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        left, right = 0, len(nums)-1
+        #find the minimum in rotated array
 
-        while left <= right:
-            mid = (left + right)//2
+        left = 0
+        right = len(nums)-1
+
+        while left<right:
+            mid = left+(right-left)//2
+            #we found the pivot element
+            if nums[mid] > nums[right]:
+                left = mid+1
+            else:
+                right = mid
+
+        min_element = left
+
+        #now to find the index of the target, do another binary search
+
+        #for already ascending non rotatd array
+        if nums[0] == nums[min_element]:
+            left, right = 0, len(nums)-1
+        elif target >= nums[min_element] and target < nums[0]:
+            left, right = min_element, len(nums)-1
+        else:
+            left, right = 0, min_element-1
+
+
+        #do traditional BS
+
+        while left<=right:
+            mid = left + (right-left)//2
 
             if target == nums[mid]:
                 return mid
-            
-            if nums[left] <= nums[mid]:# means we are in left sorted array
-                if target > nums[mid] or target < nums[left]:
-                    left = mid+1
-                else:
-                    right = mid-1 #target < nums[mid] or target > nums[left] or (target < nums[mid] and
-                    #target < nums[left])
-
-
-            else: # means we are in right sorted array
-                if target < nums[mid] or target > nums[right]:
-                    right = mid-1
-                else:
-                    left = mid+1 #target > nums[mid] or target < nums[right]
+            elif target > nums[mid]:
+                left = mid+1
+            else:
+                right = mid-1
 
         return -1
