ac67ddf6be427beeb86cb13ac5a2e0b126031a2f
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2025-09-06 12:39:55 -0700

    Time: 87 ms (5.47%), Space: 22.3 MB (35.78%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,24 +1,12 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
 
-        hashMap = {}
-
+        map = {}
+        
         for str in strs:
             count = [0]*26
-
             for char in str:
                 count[ord(char)-ord('a')] += 1
+            map.setdefault(tuple(count),[]).append(str)
 
-            key = tuple(count) 
-
-            if key not in hashMap:
-                hashMap[key] = []
-
-            hashMap[key].append(str)
-
-        result = []
-
-        for key,value in hashMap.items():
-            result.append(value)
-
-        return result
+        return map.values()    
