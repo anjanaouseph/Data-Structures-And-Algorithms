0adfa4f882289dc0a202933237edf8ce24125164
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2025-09-06 17:48:18 -0700

    Time: 17 ms (5.73%), Space: 23.9 MB (26.41%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,24 +1,32 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+
         if len(nums) == 1:
             return [1]
 
+        count = [[] for _ in range(len(nums)+1)] #indices represent the frequencies
+
         hashMap = {}
 
-        for num in nums:#O(n)
+        for num in nums:
             if num not in hashMap:
                 hashMap[num] = 0
 
             hashMap[num] += 1
 
-        sorted_items = sorted(hashMap.items(), key=lambda x: x[1], reverse=True)#this is tuples
-        #O(nlogn)
 
-        count = k
+        for key,val in hashMap.items():
+            count[val].append(key)
 
+        key = k
         res = []
 
-        for i in range(count):#O(k)
-            res.append(sorted_items[i][0])
+        for i in range(len(nums),0,-1):
+            for freq in count[i]:
+                if key:
+                    res.append(freq)
+                    key -= 1
 
         return res
+
+        
