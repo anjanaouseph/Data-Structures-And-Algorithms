223f10197548a2134138657f37898b747ae668b1
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2025-09-09 22:54:03 -0700

    Time: 38 ms (36.81%), Space: 20.3 MB (71.82%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,24 +1,17 @@
-import collections
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        #using two pointers
 
-        if len(nums) == 0:
-            return 0
-
-        prefix_sum = defaultdict(int)
+        hashMap = collections.defaultdict(int)
+        hashMap[0] = 1
         count = 0
         current_sum = 0
-        prefix_sum[0] = 1 #to handle edge case where subarray starts from index 0 or a single element array like [3] and k=3
 
-        for i in range(len(nums)):
-            current_sum += nums[i]
+        for num in nums:
+            current_sum += num
 
-            if current_sum-k in prefix_sum:
-                count += prefix_sum[current_sum-k]
+            if current_sum-k in hashMap:
+                count += hashMap[current_sum-k]
 
-            prefix_sum[current_sum] += 1
+            hashMap[current_sum] += 1
 
-        return count
-
-
+        return count         
