2f2f1b3302e6de9e32bebd6340487b34dce09e7f
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2025-09-12 23:11:17 -0700

    Time: 0 ms (100%), Space: 18.2 MB (22.66%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,41 +1,39 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        #find the minimum in rotated array
 
-        left = 0
+        left = 0 
         right = len(nums)-1
 
-        while left<right:
-            mid = left+(right-left)//2
-            #we found the pivot element
+        #first find the min element
+        while left < right:
+            mid = (left+right)//2
+
             if nums[mid] > nums[right]:
+                #unsorted half
                 left = mid+1
+
             else:
                 right = mid
 
         min_element = left
 
-        #now to find the index of the target, do another binary search
-
-        #for already ascending non rotatd array
-        if nums[0] == nums[min_element]:
-            left, right = 0, len(nums)-1
-        elif target >= nums[0] and target <= nums[min_element-1]:
-            left , right = 0, min_element-1
+        #do BS again 
+        if nums[min_element] == nums[0]:
+            left, right =0,  len(nums)-1
+        elif target >= nums[min_element] and target >= nums[0]:
+            left, right = 0, min_element-1
         else:
             left, right = min_element, len(nums)-1
 
-
-        #do traditional BS
-
         while left<=right:
-            mid = left + (right-left)//2
+            mid = (left + right)//2
 
             if target == nums[mid]:
                 return mid
+
             elif target > nums[mid]:
                 left = mid+1
             else:
                 right = mid-1
 
-        return -1
+        return -1 
