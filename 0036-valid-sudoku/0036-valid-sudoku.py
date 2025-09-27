3c03221573a2cf0bc0bdd2cd176af7e34ece6610
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2025-09-26 20:47:35 -0700

    Time: 5 ms (46.91%), Space: 17.7 MB (92.41%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,40 +1,21 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
 
-        #validate rows
+        row_set = collections.defaultdict(set)
+        col_set = collections.defaultdict(set)
+        box_set = collections.defaultdict(set)
+
         for i in range(9):
-            sett = set() #initialize a set fo every row
             for j in range(9):
-                if board[i][j] in sett:
+                if board[i][j] == '.':
+                    continue
+
+                if (board[i][j] in row_set[i] or board[i][j] in col_set[j] or board[i][j] in box_set[(i//3,j//3)]):
                     return False
 
-                elif board[i][j] != ".":
-                    sett.add(board[i][j])
-
-        #validate cols
-        for i in range(9):
-            sett = set()
-            for j in range(9):
-                if board[j][i] in sett:
-                    return False
-
-                elif board[j][i] != ".":
-                    sett.add(board[j][i])
-
-
-        #validate boards
-        directions = [(0,0), (0,3), (0,6),
-                    (3,0), (3,3), (3,6),
-                    (6,0), (6,3), (6,6)]
-
-        for x,y in directions:
-            sett = set() #reset after every board
-            for i in range(x,x+3):
-                for j in range(y,y+3):
-                    if board[i][j] in sett:
-                        return False
-
-                    elif board[i][j] != ".":
-                        sett.add(board[i][j])
+                else:
+                    row_set[i].add(board[i][j])
+                    col_set[j].add(board[i][j])
+                    box_set[(i//3,j//3)].add(board[i][j])
 
         return True
