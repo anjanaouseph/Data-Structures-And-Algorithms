9f6965a3a8eeae56fc65078d4b1ddfe127859e94
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2025-10-15 23:20:48 -0700

    Time: 95 ms (51.93%), Space: 26.8 MB (77.85%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,17 +1,20 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        #We need to buy low and sell high
-        l,r = 0,1
-        max_profit = 0
-        while r<len(prices):
 
-            if prices[l] < prices[r]:
-                profit = prices[r] - prices[l]
-                max_profit = max(max_profit, profit)
+        low = 0
+        high = 1
+
+        max_profit = 0
+
+        while high < len(prices):
+            if prices[low] < prices[high]:
+                profit = prices[high] - prices[low]
+
+                max_profit = max(profit, max_profit)
+
             else:
-                l = r #we know l>=r thats why we came here, if we keep l at r then anyways the difference will be more, meaning more profit. We can optimize in this way.
-#eg: Why buy on day 1 when I can buy on day 2 for a lesser price (intention is to maximise the profit)
-#NOTE: We want out left pointer to always be at the minimum
-            r += 1
+                low = high #which means we found another day that is lower than low so buy on that  day instead of on the prev low day.
+
+            high += 1
+
         return max_profit
-        
