b47fdad12627ca24571a5a3548c68159df681ada
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2025-10-18 23:42:43 -0700

    Time: 231 ms (13.99%), Space: 18 MB (49.2%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -6,13 +6,14 @@ class Solution:
         left = 0
         right = 0
 
-        char_map = [0]*26
+        char_map = [0]*26 #to keep track of the count of characters in a particular window.
 
         while right < len(s):
 
             char_map[ord(s[right])-ord('A')] += 1
 
             while (right-left+1)-max(char_map)>k: #means the window is not satisfying
+            #max(char_map) scans 26 entries; that’s O(26)=O(1).
                 char_map[ord(s[left])-ord('A')] -= 1
                 left += 1
 
