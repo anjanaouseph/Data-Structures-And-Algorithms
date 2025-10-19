f6505e6388aaa35050e7965ccf0aef6ed3659dd4
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2025-10-18 23:21:14 -0700

    Time: 224 ms (15.46%), Space: 18.1 MB (23.5%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,24 +1,22 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        #first optimized solution using sliding window
 
-        l,r = 0,0
-        max_freq, max_length = 0,0
-        map = [0]*26#O(26) space complexity
+        longest = 0
 
-        while r < len(s):
-            map[ord(s[r]) - ord('A')] +=  1
-            max_freq = max(max_freq, map[ord(s[r])-ord('A')])
+        left = 0
+        right = 0
 
-            if (r-l+1 - max_freq) > k:#when the charaters to replace exceeds K
-                map[ord(s[l])-ord('A')] -= 1 #we decrease the count
-                #dont recalculate max_freq here again because decreasing that is pointless so
-                #like this we can remove o(26) dependency, makes it a bit faster.
-                l += 1 #shrink the window
+        char_map = [0]*26
 
-            if r-l+1 - max_freq <=k :
-                max_length = max(max_length, r-l+1) #calculate max length
-                r += 1
+        while right < len(s):
 
-        return max_length
-        #Time Complexity is O(n)+O(n)*O(26) which is  O(n)+O(n)*O(1) which is O(n)
+            char_map[ord(s[right])-ord('A')] += 1
+
+            while (right-left+1)-max(char_map)>k: #means the window is not satisfying
+                char_map[ord(s[left])-ord('A')] -= 1
+                left += 1
+
+            longest = max(longest, right-left+1)
+            right += 1
+        
+        return longest
