9f9023adf5a898a79a671253bfe5f8d636e0af7c
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2025-11-28 21:21:32 -0800

    Time: 11 ms (59.19%), Space: 20.2 MB (56.36%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -16,19 +16,16 @@ class Solution:
         dummy = ListNode()
         curr = dummy
 
-        for i, node in enumerate(lists):
+        for i, node in enumerate(lists): #o(k)
             if node:
                 heapq.heappush(heap, (node.val, i, node)) #we push the head of each LL not all elements. so K elements at a time in heap., we need node value to get next element
 
-        while heap:
-            val, i, node = heapq.heappop(heap)
+        while heap: #loop runs exactly N times.
+            val, i, node = heapq.heappop(heap) 
             curr.next = node
             curr = node
             node = node.next
             if node:
                 heapq.heappush(heap, (node.val, i, node))
 
-        return dummy.next
-
-
-    
+        return dummy.next    
