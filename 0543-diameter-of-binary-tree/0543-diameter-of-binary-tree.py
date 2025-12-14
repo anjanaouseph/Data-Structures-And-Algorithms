5b65524c2f9ffe7eef8c8749ac9054ac142203b7
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2025-12-14 10:02:00 -0800

    Time: 7 ms (46.6%), Space: 21 MB (21.06%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
@@ -6,26 +6,19 @@
 #         self.right = right
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        #find height using DFS
-        #diameter = left_height + right_height
         largest_diameter = [0]
 
-        def height(root):#The height function is a nested function inside diameterOfBinaryTree, so it does not belong to the Solution class. Adding self as a parameter is unnecessary and will result in an error when calling it.
-                if not root:
-                    return 0
+        def height(node):
+            if not node:
+                return 0
 
-                left = height(root.left)
-                right = height(root.right)
-                diameter = left + right
-                largest_diameter[0] = max(largest_diameter[0], diameter)
-                #
+            left_height = height(node.left)
+            right_height = height(node.right)
+            diameter = left_height + right_height
 
-                return max(left,right)+1
+            largest_diameter[0] = max(diameter, largest_diameter[0])
+
+            return 1+max(left_height, right_height)
 
         height(root)
-        return largest_diameter[0]
-
-       
-
-
-        
+        return largest_diameter[0]  
