54158185ecde147637b611f3cb626d131de29e6d
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2025-12-26 17:37:11 -0800

    Time: 0 ms (100%), Space: 17.5 MB (97.25%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,39 +1,26 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
+        low = 0
+        high = len(nums)-1
 
-        left = 0 
-        right = len(nums)-1
+        while low <= high:
+            mid = low + (high - low)//2 #to take care of integer overflow    
 
-        #first find the min element
-        while left < right:
-            mid = (left+right)//2
-
-            if nums[mid] > nums[right]:
-                #unsorted half
-                left = mid+1
-
-            else:
-                right = mid
-
-        min_element = left
-
-        #do BS again 
-        if nums[min_element] == nums[0]:
-            left, right =0,  len(nums)-1
-        elif target >= nums[min_element] and target >= nums[0]:
-            left, right = 0, min_element-1
-        else:
-            left, right = min_element, len(nums)-1
-
-        while left<=right:
-            mid = (left + right)//2
-
-            if target == nums[mid]:
+            if nums[mid] == target:
                 return mid
+            #check left or right half is sorted
+            if nums[low] <= nums[mid]:
+                #now check if the target is in this range
+                if nums[low] <= target and nums[mid]>target:
+                    high = mid-1
+                else:
+                    low = mid+1
 
-            elif target > nums[mid]:
-                left = mid+1
             else:
-                right = mid-1
+                #means right side is sorted
+                if nums[mid] < target and nums[high] >= target:
+                    low = mid+1
+                else:
+                    high = mid-1
 
-        return -1 
+        return -1        
