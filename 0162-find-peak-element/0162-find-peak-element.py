ff1a85dce7f8046028f2b8049e6ca24c523577fa
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2025-12-27 13:37:13 -0800

    Time: 0 ms (100%), Space: 17.3 MB (94.96%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.py
+++ 0162-find-peak-element/0162-find-peak-element.py
@@ -3,18 +3,16 @@ class Solution:
         low = 0
         high = len(nums)-1
 
-        while low <= high:
+        while low < high:
             mid = low + (high-low)//2
 
             if (mid == 0 or nums[mid] > nums[mid-1]) and (mid == len(nums)-1 or nums[mid] > nums[mid+1]):
                 return mid #checking boundary conditions and peak element condition
 
-            elif nums[mid+1] > nums[mid]:
+            elif mid < len(nums)-1 and nums[mid+1] > nums[mid]:
                 low = mid+1
             else:
-                high = mid-1
+                high = mid #here we don't need boundary check. loop exits
 
-        return -1
-
-            
+        return low
         
