60420e68052ba10c9521881e098c877fcba36d24
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2025-12-30 15:45:13 -0800

    Time: 7 ms (58.04%), Space: 17.4 MB (92.04%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,29 +1,24 @@
 #can maintain mapping in LL and arry list also but for
 #O(1) search complexity, we need hashMap
 #we need s to t and t to s mapping to avoid breach of "No two characters may map to the same character". eg: egl and add , here g and l can map to d. To avoid this we need 2 hashmaps
-#we can't have a single map for s->t and t->s mapping because we won't know from which string the chars are coming from. Else we have to maintain a flag with keys to show distinction but its as good as having two hashmaps. space complexity will be 2 * no. of keys, if we use 2 hashmaps or two keys within a single hashmap. 
+#we can't have a single map for s->t and t->s mapping because we won't know from which string the chars are coming from. Else we have to maintain a flag (in a tuple with key) with keys to show distinction but its as good as having two hashmaps. space complexity will be 2 * no. of keys, if we use 2 hashmaps or two keys within a single hashmap. 
 
-from collections import defaultdict
 
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
 
-        hashMap_s = defaultdict(str)
-        hashMap_t = defaultdict(str)
+        s_to_t = {}
+        t_to_s = {}
 
-        for i in range(len(s)):
-            if hashMap_s[s[i]]:
-                if hashMap_s[s[i]] != t[i]:
-                    return False #breach
-            else:
-                hashMap_s[s[i]] = t[i]
-            
-            if hashMap_t[t[i]]:
-                if hashMap_t[t[i]] != s[i]:
-                    return False #breach
-            else:
-                hashMap_t[t[i]] = s[i]
+        for cs, ct in zip(s, t):
+            if cs in s_to_t and s_to_t[cs] != ct:
+                return False
+            if ct in t_to_s and t_to_s[ct] != cs:
+                return False
+
+            s_to_t[cs] = ct
+            t_to_s[ct] = cs
 
         return True
