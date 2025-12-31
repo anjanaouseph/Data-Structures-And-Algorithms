cd24f25c255ff2b00442f394673ed62f498b08f4
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2025-12-31 01:08:46 -0800

    Time: 15 ms (99.75%), Space: 20.3 MB (86.75%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,4 +1,12 @@
-#need hashmap to store the occurances of the sum not index
+#need hashmap to store the occurances of the sum not index here.
+# We check prefix_sum - k because if the current prefix sum is prefix_sum, any earlier prefix sum equal to prefix_sum - k forms a subarray ending here with sum k
+
+# Why NOT check prefix sum directly? Only tells you:“Have I seen this running sum before?”
+# That corresponds to subarrays with sum 0, not k.
+
+# Why NOT check k directly?
+# “Did some prefix sum equal k?”
+# That only finds subarrays starting at index 0.
 
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
