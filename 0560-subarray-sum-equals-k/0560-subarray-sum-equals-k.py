ed934994b040fbb3e5368b7130259f76e6fe19ad
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2025-12-31 00:34:33 -0800

    Time: 27 ms (81.96%), Space: 20.4 MB (65.13%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,19 +1,17 @@
+#need hashmap to store the occurances of the sum not index
+
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
 
-       hashMap = collections.defaultdict(int)
-       hashMap[0] = 1 
+        hashMap = {0 : 1}
+        prefix_sum = 0
+        count = 0
 
-       running_sum = 0
-       res = 0
+        for num in nums:
+            prefix_sum += num
 
-       for num in nums:
-            running_sum += num
+            if prefix_sum - k in hashMap:
+                count += hashMap[prefix_sum-k]
+            hashMap[prefix_sum] = hashMap.get(prefix_sum, 0)+1
 
-            if hashMap[running_sum - k]:
-                res += hashMap[running_sum-k]
-                #running_sum-k can be zero when [1 2] and k = 3 to handle that we need to
-                #set hashMap[0] = 1 
-
-            hashMap[running_sum] += 1
-       return res
+        return count       
