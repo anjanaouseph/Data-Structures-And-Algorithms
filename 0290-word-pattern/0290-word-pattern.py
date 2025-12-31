c7e29a1d934fca661f43839f9f278c93720cc289
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2025-12-30 16:16:12 -0800

    Time: 0 ms (100%), Space: 17.2 MB (98.17%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.py
+++ 0290-word-pattern/0290-word-pattern.py
@@ -1,14 +1,16 @@
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
-        words = s.split() #O(N)
+        words = s.split() #O(N) N is the length of string
+
+        #M is the no of words or length of pattern. words O(M) space complexity
 
         if len(pattern) != len(words):
             return False
 
-        char_to_word = {}
-        word_to_char = {}
+        char_to_word = {} #O(M) time and space
+        word_to_char = {} #O(M) time and space
 
-        for ch, word in zip(pattern, words):
+        for ch, word in zip(pattern, words): #O(M)
             if ch in char_to_word and char_to_word[ch] != word:
                 return False
             
