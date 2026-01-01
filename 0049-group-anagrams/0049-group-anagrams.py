d6a56a4a8018668725031f160fb1995fb1aa5305
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2025-12-31 21:33:34 -0800

    Time: 19 ms (37.78%), Space: 21.9 MB (32.98%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,23 +1,23 @@
-from collections import defaultdict
-
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         if not strs:
-            return None
+            return []
 
-        hashMap = defaultdict(list)
+        hashMap = {}
         res = []
 
         for str in strs:
-            #26 length array initialize
-            key = [0]*26
+            count = [0]*26
 
-            for char in str:
-                key[ord(char)-ord('a')] += 1
+            for ch in str:
+                count[ord(ch)-ord('a')] += 1
 
-            hashMap[tuple(key)].append(str)
+            key = tuple(count)
+            if key not in hashMap:
+                 hashMap[key] = []
+            hashMap[key].append(str)
 
         for key,values in hashMap.items():
             res.append(values)
-
-        return res
+        
+        return res      
