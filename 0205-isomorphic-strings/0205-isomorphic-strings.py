3ddbb3142381e9115cfc1cda3909e22dc772184c
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2025-12-31 21:51:33 -0800

    Time: 16 ms (5.94%), Space: 17 MB (99.87%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,24 +1,18 @@
-#can maintain mapping in LL and arry list also but for
-#O(1) search complexity, we need hashMap
-#we need s to t and t to s mapping to avoid breach of "No two characters may map to the same character". eg: egl and add , here g and l can map to d. To avoid this we need 2 hashmaps
-#we can't have a single map for s->t and t->s mapping because we won't know from which string the chars are coming from. Else we have to maintain a flag (in a tuple with key) with keys to show distinction but its as good as having two hashmaps. space complexity will be 2 * no. of keys, if we use 2 hashmaps or two keys within a single hashmap. 
-
-
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
 
-        s_to_t = {}
-        t_to_s = {}
+        hashMap_s = {}
+        hashMap_t = {}
 
-        for cs, ct in zip(s, t):
-            if cs in s_to_t and s_to_t[cs] != ct:
+        for i in range(len(s)):
+            if s[i] in hashMap_s and hashMap_s[s[i]] != t[i]:
+                return False #breach
+            if t[i] in hashMap_t and hashMap_t[t[i]] != s[i]:
                 return False
-            if ct in t_to_s and t_to_s[ct] != cs:
-                return False
 
-            s_to_t[cs] = ct
-            t_to_s[ct] = cs
+            hashMap_s[s[i]] = t[i]
+            hashMap_t[t[i]] = s[i]
 
-        return True
+        return True       
