6dd61f5c5a847170d319d2144d4b9942015b54f3
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2026-01-03 15:14:59 -0800

    Time: 55 ms (72.73%), Space: 20.8 MB (75.33%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -7,21 +7,33 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        lca = [root]
+        #iterative solution in DFS
+        if not root or not q or not p:
+            return None
 
-        def search(root):
-            if not root:
-                return
+        stack = [root]
 
-            lca[0] = root
-            if root is p or root is q: #case where either p or q is root
-                return
-            elif root.val > p.val and root.val > q.val: #case where both are on left subtree
-                search(root.left)
-            elif root.val < p.val and root.val < q.val: #case where both are on right subtree
-                search(root.right)
-            else:
-                return #case where they both are on either side of the root, so LCA will be the root
+        while stack:
+            size = len(stack)
+            for i in range(size):
+                root = stack.pop()
 
-        search(root)
-        return lca[0]
+                if root.val == p.val or root.val == q.val:
+                    return root
+                elif p.val > root.val and q.val > root.val:
+                    if root.right:
+                        stack.append(root.right)
+                elif p.val < root.val and q.val < root.val:
+                    if root.left:
+                        stack.append(root.left)
+                else:
+                    return root
+
+        return None
+
+
+
+
+
+
+        
