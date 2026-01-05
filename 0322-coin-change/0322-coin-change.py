379d810e10b222a38b9990f16836cdca4a869dbb
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2026-01-04 21:52:16 -0800

    Time: 965 ms (27.82%), Space: 17.5 MB (99.4%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,21 +1,23 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
+        #optimizing using 1D array
+        #basically we are just overwritting each row
         m = len(coins) #rows
         n = amount #cols
 
-        #create a 2D matrix of (m+1)*(n+1) dimension
-        dp = [[0] * (n+1) for _ in range(m+1)] #we will have m+1 and n+1 rows and cols because we added a 0th column and 0th row above
+        #create a 1D array of (n+1) dimension
+        dp = [0] * (n+1)
 
         for j in range(1,n+1):
-            dp[0][j] = float('inf') #dummy row except first cell rest all are infs
+            dp[j] = float('inf') 
 
         #now lets fill the actual table
         for i in range(1,m+1):#skip first row which is the dummy row
             for j in range(n+1): #cant skip first column.
                 if j < coins[i-1]: #amount < coin then not possible i-1 to map to index in original coins array because now we have a preceeding 0 in coins array due to dummy row [0 1 2 5] instead of [1 2 5]
-                    dp[i][j] = dp[i-1][j] #copy the value from the prev row as it is
+                    dp[j] = dp[j] #copy the value from the prev row as it is
                 else:
-                    dp[i][j] = min(dp[i-1][j], 1+dp[i][j - coins[i-1]]) #if condition above ensures dp[i][j - coins[i-1] is not out of bounds. as we are checking if j < coins[i-1] case.
+                    dp[j] = min(dp[j], 1+dp[j - coins[i-1]]) #if condition above ensures dp[j - coins[i-1] is not out of bounds. as we are checking if j < coins[i-1] case.
                     #taking the min of : case where we don't choose that coin and case where we choose the coin + 1. 
 
-        return -1 if dp[m][n] == float('inf') else dp[m][n]     
+        return -1 if dp[n] == float('inf') else dp[n]     
