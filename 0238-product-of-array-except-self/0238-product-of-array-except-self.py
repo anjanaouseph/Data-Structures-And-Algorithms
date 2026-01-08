746ae4dd21106779f0db98cdbd5a22a442f38e23
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2026-01-07 18:14:58 -0800

    Time: 19 ms (87.09%), Space: 25.7 MB (26.27%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,19 +1,20 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+        result = [0]*len(nums)
+        #take the running sum of products. For an integer nums[i] take left running sum and then find its right running sum and multiply them both together.
 
-        #Solution with O(N) time and O(1) space complexity
-        n = len(nums)
-        res = [1]*n
+        #taking the left side running sum
+        rp = 1
 
-        pdt_left = 1
-        pdt_right = 1
+        for i,num in enumerate(nums):
+            result[i] = rp
+            rp = num*rp
 
-        for i in range(n):
-            res[i] = pdt_left
-            pdt_left *= nums[i]
+        #taking the right side running sum
+        rp = 1
 
-        for i in range(n-1, -1, -1):
-            res[i] *= pdt_right
-            pdt_right *= nums[i]
+        for i in range (len(nums)-1, -1, -1):
+            result[i] *= rp
+            rp *= nums[i]
 
-        return res       
+        return result
