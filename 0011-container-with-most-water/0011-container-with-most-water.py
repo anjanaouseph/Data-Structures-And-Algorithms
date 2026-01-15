c6450098c2972f6fa27835d07eeaef9f237c9f0d
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2026-01-15 14:32:06 -0800

    Time: 63 ms (82.4%), Space: 29.8 MB (6.37%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,19 +1,25 @@
+#limiting factors are width and min(h1,h1), the min height. So we start with max width possible and the
+#move the pointers accordingly to maximize the area possible.
+#after we have explored the max water possible with a pointer (left or right), we move it.
+
 class Solution:
     def maxArea(self, height: List[int]) -> int:
 
-        max_water = 0
+        max_area = 0
 
-        left = 0
-        right = len(height)-1
+        left , right = 0, len(height)-1
 
-        while left < right:
-            area = min(height[left], height[right])*(right-left) #start at max width
+        while left <= right: #it can be left < right also
+            area = min(height[left], height[right]) * (right-left)
 
-            max_water = max(max_water, area)
+            max_area = max(area, max_area)
 
-            if height[left]<= height[right]:#we try to maximize the smaller constraint in the aim to find the bigger area
+            if height[left] < height[right]:
                 left += 1
             else:
-                right -= 1
+                right -=1
 
-        return max_water     
+        return max_area
+
+
+        
