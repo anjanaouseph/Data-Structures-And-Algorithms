15f8e9fa75b26aa7a139f65c1b8aba8a5437602b
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2026-01-15 15:10:07 -0800

    Time: 63 ms (82.4%), Space: 29.8 MB (6.37%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -19,3 +19,11 @@ class Solution:
                 right -=1
 
         return max_area
+
+# The two-pointer approach does not necessarily compute the maximum area for each individual height.
+# It still finds the global maximum, because it only evaluates the pairs that could possibly beat the best-so-far.
+# example left = 2, right = 8 width = 50
+# min(2,8) * 50 = 2*50 = 100
+# we need to beat this 100. The only way we can beat 100 is to move left pointer, if left pointer is some value say 8 
+# then min(8,8)= 8*49
+# if we move right pointer max we can get is min(2,2)*49. which is 2*49 at best we can find.
