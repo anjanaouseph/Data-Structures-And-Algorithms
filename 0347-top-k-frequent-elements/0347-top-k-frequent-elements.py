e4ff39a43ef9ccf3c0ac53d8e498914d352f4353
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2026-01-20 10:44:10 -0800

    Time: 15 ms (11.04%), Space: 24.7 MB (7.67%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,32 +1,38 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-
-        if len(nums) == 1:
-            return [1]
-
-        count = [[] for _ in range(len(nums)+1)] #indices represent the frequencies
-
-        hashMap = {}
-
-        for num in nums:
-            if num not in hashMap:
-                hashMap[num] = 0
-
-            hashMap[num] += 1
-
-
-        for key,val in hashMap.items():
-            count[val].append(key)
-
-        key = k
-        res = []
-
-        for i in range(len(nums),0,-1):
-            for freq in count[i]:
-                if key:
-                    res.append(freq)
-                    key -= 1
-
-        return res
-
+        if not nums:
+            return []
         
+        hashMap = defaultdict(int)
+        
+        result = []
+        
+        max_val = 0
+        min_val = float('inf')
+        
+        for num in nums:
+            hashMap[num] = hashMap.get(num,0)+1
+            max_val = max(hashMap[num], max_val)
+            min_val = min(hashMap[num], min_val)
+            
+        
+        # 1:3 2:2 3:1
+        bucket_freq = [[] for _ in range(len(nums) + 1)]
+
+         
+        for key,value in hashMap.items():
+            bucket_freq[value].append(key)
+ 
+            
+        count = k
+            
+        for i in range(max_val, -1, -1):
+            if bucket_freq[i] and count > 0:
+                for freq in bucket_freq[i]:
+                    if count>=0:
+                        result.append(freq)
+                        count -= 1
+                    
+        return result
+    
+    #meeting room
