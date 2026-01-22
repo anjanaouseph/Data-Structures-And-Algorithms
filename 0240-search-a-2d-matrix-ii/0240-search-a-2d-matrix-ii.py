4e7e8539513130bca1cbc4e29bec0950982de09d
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2026-01-21 16:30:46 -0800

    Time: 137 ms (82.99%), Space: 25.9 MB (6.49%) - LeetHub

---
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.py
@@ -0,0 +1,30 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        #here ful 2D matrix is not sorted. Only rows are sorted.
+        #so can't consider as a 1-D array.
+        #we use steps method O(m+n)
+        #can do BS on rows O(m*logn)
+        #can do BS on column O(n*logm)
+        #brute force is O(m*n)
+        
+        #in steps method start from top right or bottom left cell.
+        #other two corners we can't make a decision in which way to proceed as both sides will be either smaller or greater
+
+        #starting from top right corner here
+
+        m = len(matrix)
+        n = len(matrix[0])
+
+        #right most corner
+        row = 0
+        col = n-1
+
+        while 0 <= row < m and 0 <= col < n:
+            if target == matrix[row][col]:
+                return True
+            elif target > matrix[row][col]:
+                row += 1
+            else:
+                col -= 1
+
+        return False
