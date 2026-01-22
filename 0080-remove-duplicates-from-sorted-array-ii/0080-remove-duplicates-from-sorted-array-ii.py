6858aece65b50a374e6313ee0ba518732261f709
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2026-01-21 18:59:57 -0800

    Time: 87 ms (43.34%), Space: 22.4 MB (6.7%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
@@ -12,7 +12,7 @@ class Solution:
 
             curr = nums[fast]
 
-            while (fast < len(nums) and nums[fast] == curr):
+            while (fast < len(nums) and nums[fast] == curr): #so slow is a pointer which helps overwrite array, if u do nums[fast] == nums[slow], it can go to infinite loop in case like this [1 1 1 2 2 3], where slow = nums[2] = [1] and fast= nums[3] = [2], count = 0, both fast and slow won't incrementy. So infinite loop
                 fast += 1
                 count += 1
 
@@ -26,3 +26,6 @@ class Solution:
             count = 0
 
         return slow
+
+    # Time Complexity: O(2N) = O(N)
+    # Space Complexity: O(1)
