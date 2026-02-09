19a98f45ced82acb84e40d200eba970a3fab445a
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2026-02-09 14:42:07 -0700

    Time: 0 ms (100%), Space: 19.5 MB (14.59%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.py
+++ 0226-invert-binary-tree/0226-invert-binary-tree.py
@@ -9,15 +9,15 @@ class Solution:
         if not root:
             return None
 
-        queue = deque([root])
+        stack = [root]
 
-        while queue:
-            node = queue.popleft()
+        while stack:
+            node = stack.pop()
             node.left, node.right = node.right, node.left
 
             if node.left:
-                queue.append(node.left)
+                stack.append(node.left)
             if node.right:
-                queue.append(node.right)
+                stack.append(node.right)
 
         return root
