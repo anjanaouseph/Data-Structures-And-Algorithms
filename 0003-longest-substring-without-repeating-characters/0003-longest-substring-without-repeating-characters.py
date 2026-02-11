051326c05f12cf521863a70a600d2c56835ce4c7
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2026-02-11 12:50:20 -0700

    Time: 8 ms (91.61%), Space: 19.6 MB (5.28%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -5,12 +5,12 @@ class Solution:
 
         hashMap = {}
         max_len = 0
-        slow = 0
+        slow = 0 #marks the start of the current valid substring
 
         for i in range(len(s)):
             c = s[i]
             if c in hashMap:
-                slow = max(slow, hashMap[c]+1)
+                slow = max(slow, hashMap[c]+1) #if slow ptr has gone past the index of c, then take the current pos of slow ptr, meaning the duplicate is not part of current substring, else jump slow to index+1
 
             hashMap[c] = i
 
@@ -18,5 +18,10 @@ class Solution:
 
         return max_len
 
+# Time Complexity : O(N)
+# Space Complexity: O(1)
 
+# If the duplicate character was seen inside the current window, we move slow past its previous index.
+# If it was seen before the current window, we do nothing and keep slow where it is.
+# This prevents slow from ever moving backward.
         
