40f631e485d10a30ccf268d3c5822a7afac20180
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2026-02-11 14:08:18 -0700

    Time: 1 ms (22.72%), Space: 19.4 MB (21.38%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.py
+++ 0791-custom-sort-string/0791-custom-sort-string.py
@@ -7,29 +7,23 @@ class Solution:
 
         result = []
 
-        for i in range(len(s)):
+        for i in range(len(s)): #get count of all chars in s
             ch = s[i]
             hashMap[ch] = hashMap.get(ch, 0)+1
 
-        for i in range(len(order)):
+        for i in range(len(order)): #get each char in order and append to result
             ch = order[i]
 
             while hashMap.get(ch, 0) != 0: #if ch is in order but not in s raises error
                 result.append(ch)
                 hashMap[ch] -= 1
 
-        for key in hashMap.keys():
+        for key in hashMap.keys():#append remaining elements
             while hashMap[key] != 0:
                 result.append(key)
                 hashMap[key] -= 1
 
-        return "".join(result)
-                
+        return "".join(result)        
 
-        
-
-
-
-
-
-        
+# Time Complexity: O(N)
+# Space Complexity: O(1) (excluding the output string, since the hashmap is bounded by a fixed alphabet size)
