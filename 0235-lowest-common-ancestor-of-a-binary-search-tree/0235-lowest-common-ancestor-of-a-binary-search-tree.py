14a3824bdb2dbe49a1276639f04e4d26b51f73b3
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2026-02-10 19:20:42 -0700

    Time: 70 ms (18.12%), Space: 22.9 MB (25.94%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -7,20 +7,29 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        lca = [root]
 
-        def search(root, p, q):
+        if not p and not q:
+            return -1
+
+        if not p or not q:
+            return -1
+
+        ancestor = [root]
+
+        def lca(root, p ,q):
             if not root:
-                return None
-            lca[0] = root
-            if p.val > root.val and q.val > root.val:
-                search(root.right,p, q)
-            elif p.val < root.val and q.val < root.val:
-                search(root.left,p, q)
-            else:
-                return #case where both branches diverge and where p == root or q == root
+                return
 
-        search(root,p,q)
-        return lca[0]
-                
-            
+            ancestor[0] = root
+
+            if p.val > root.val and q.val > root.val:
+                lca(root.right,p, q)
+
+            elif p.val < root.val and q.val < root.val:
+                lca(root.left, p, q)
+
+            else:
+                return 
+
+        lca(root,p, q)
+        return ancestor[0]
