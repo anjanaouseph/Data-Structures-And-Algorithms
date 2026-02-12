c2369d90ac5907fe38770278738e3212185078a2
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2026-02-11 19:41:16 -0700

    Time: 141 ms (29.31%), Space: 31.6 MB (84.06%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
@@ -7,25 +7,21 @@
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
         if not root:
-            return 0
+            return False
 
-        #dfs
         stack = [(root, root.val)]
 
         count = 0
 
         while stack:
-            node, max_val = stack.pop()
+            while len(stack):
+                node, max_val = stack.pop()
+                if node.val >= max_val:
+                    count += 1
+                max_val = max(node.val, max_val)
+                if node.left:
+                    stack.append((node.left, max_val))
+                if node.right:
+                    stack.append((node.right, max_val))
 
-            if node.val >= max_val:
-                count += 1
-
-            if node.left:
-                stack.append((node.left, max(node.left.val, max_val)))
-            if node.right:
-                stack.append((node.right, max(node.right.val, max_val)))
-
-        return count       
-
-# Time Complexity: O(N)
-# Space Complexity: O(H) which is O(N) for skewed trees and O(logN) for complete trees 
+        return count 
