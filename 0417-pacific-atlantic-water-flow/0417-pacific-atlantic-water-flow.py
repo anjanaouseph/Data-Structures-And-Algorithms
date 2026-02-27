404ee5abb36be17deba57659417547eefe6872c1
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2026-02-27 12:26:15 -0700

    Time: 27 ms (75.06%), Space: 20.8 MB (74.68%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,60 +1,50 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        #using multi-source bfs here 
+
         m = len(heights)
         n = len(heights[0])
 
-        visited_a = set()
-        visited_p = set()
+        a_queue = deque()
+        p_queue = deque()
 
-        #we need to do a BFS
+        a_visited = set()
+        p_visited = set()
 
-        queue_a = deque()
-        queue_p = deque()
+        directions = [(0,1), (0,-1), (1,0), (-1,0)]
 
-        #add first col to pacific ocean
-        for i in range(m):
-            queue_p.append((i,0))
-            visited_p.add((i,0))
+        #add first row to p queue
+        for j in range(n):
+            p_queue.append((0,j))
+            p_visited.add((0,j))
 
-        #first row add
-        for j in range(1,n):
-            queue_p.append((0,j))
-            visited_p.add((0,j))
+        #add first col to p queue
+        for i in range(0,m):
+            p_queue.append(((i,0)))
+            p_visited.add((i,0))
 
-         #add last col to atlantic ocean
-        for i in range(m):
-            queue_a.append((i,n-1))
-            visited_a.add((i,n-1))
+        #add last row to a_queue
+        for j in range(0,n):
+            a_queue.append((m-1,j))
+            a_visited.add((m-1,j))
 
-        #last row add
-        for j in range(0,n-1):
-            queue_a.append((m-1,j))
-            visited_a.add((m-1,j))
-
-        directions = [(-1, 0), (1,0), (0,-1), (0, 1)]
+        #add last col to a_queue
+        for i in range(0, m-1):
+            a_queue.append((i,n-1))
+            a_visited.add((i,n-1))
 
         def bfs(queue, visited):
             while queue:
-                i, j = queue.popleft()
-                for dx, dy in directions:
-                    rx,ry = dx+i, dy+j
-                    if 0<=rx<m and 0<=ry<n and (rx,ry) not in visited and heights[i][j]<= heights[rx][ry]:
-                        queue.append((rx,ry))
-                        visited.add((rx,ry))
+                cell = queue.popleft()
 
-        bfs(queue_a,visited_a)
-        bfs(queue_p,visited_p)
+                for dr,dc in directions:
+                    row = cell[0] + dr
+                    col = cell[1] + dc
+                    if 0 <= row < m and 0<=col<n and (row,col) not in visited and heights[row][col] >= heights[cell[0]][cell[1]]:
+                        visited.add((row,col))
+                        queue.append((row,col))
 
-        return list(visited_a.intersection(visited_p))
-
-                 
-        
+            return visited
 
 
-        
-
-
-        
-
-
-        
+        return list(bfs(p_queue,p_visited).intersection(bfs(a_queue,a_visited)))
