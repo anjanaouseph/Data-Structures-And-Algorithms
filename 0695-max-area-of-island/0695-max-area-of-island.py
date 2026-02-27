f162e12950e9e33e548aa4174654ba8c7ab146a3
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2026-02-26 17:48:04 -0700

    Time: 23 ms (46.39%), Space: 20.4 MB (54.14%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,31 +1,30 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        #4-directionallu. This means you only check if a "1" is directly above, below, to the left, or to the right of another "1" to determine if they are part of the same island.
-        #8 directionally means it involves diagonally connections as well.
-        #"4-directional" only considers the four main compass directions
 
-        m,n = len(grid),len(grid[0])
+        m = len(grid)
+        n = len(grid[0])
 
-        def dfs(i,j):
-            if i<0 or i>=m or j<0 or j>=n or grid[i][j] != 1:
+        max_count = 0
+        count = [0]
+
+        dir = [(1,0), (0,1), (0,-1), (-1,0)]
+
+        def dfs(r,c):
+            if r<0 or r>m-1 or c<0 or c>n-1 or grid[r][c] != 1:
                 return
-            
-            grid[i][j] = 0
-            nonlocal area_islands
-            area_islands += 1
-            dfs(i,j+1)
-            dfs(i+1, j)
-            dfs(i-1, j)
-            dfs(i, j-1)
 
+            grid[r][c] = 0
+            count[0] += 1
 
+            for dR, dC in dir:
+                dfs(r+dR, c+dC)
 
-        max_area = 0
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 1:
-                    area_islands = 0
                     dfs(i,j)
-                    max_area = max(max_area, area_islands)
+                    max_count = max(max_count, count[0])
+                    count[0] = 0
 
-        return max_area        
+        return max_count
+        
