d09f807d4c456f215bcf38b3ebd3be1d2b9cd565
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2026-02-27 18:06:37 -0700

    Time: 47 ms (40.63%), Space: 26 MB (21.9%) - LeetHub

--- 0286-walls-and-gates/0286-walls-and-gates.py
+++ 0286-walls-and-gates/0286-walls-and-gates.py
@@ -31,4 +31,20 @@ class Solution:
                 addRoom(r-1,c)
                 addRoom(r,c+1)
                 addRoom(r,c-1)
-            distance += 1 #increment by 1        
+            distance += 1 #increment by 1   
+#First elements in queue will have distance 0 #gates 
+#next set which we are adding in addRoom() will have their distance incremented by 1.    
+
+# Python follows a top-to-bottom execution model, which means:
+# Code is parsed and executed line by line in the order it appears.
+# Functions must be defined before they are called.
+# Variables must be assigned before they are used.
+
+# BFS from Empty Rooms – Inefficient Approach
+# Start a new BFS for every empty room (INF).
+# For each empty room:
+# BFS could potentially explore the entire grid to find the nearest gate.
+# In the worst case, every empty room could scan all cells.
+# Worst-Case Time Complexity:
+# There are O(m × n) empty rooms. Worse case scenario is all cells are empty rooms
+# So Each BFS can visit O(m × n) cells. O((m×n)×(m×n))=O((m×n)^2)
