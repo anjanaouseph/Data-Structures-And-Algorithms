aed1498f69ec4a5eb918db1e97dc91d17ab16c1a
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2026-02-28 12:38:39 -0700

    Time: 10 ms (26.57%), Space: 22.4 MB (69.49%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -8,28 +8,23 @@ class Solution:
 
         #add the surrounding 0s to queue and do BFS and mark as T
 
-        queue = deque()
         directions = [(1,0), (-1,0), (0,1), (0,-1)]
+
+        def dfs(r,c):
+            if r < 0 or r>m-1 or c<0 or c>n-1 or board[r][c] != 'O':
+                return
+            
+            board[r][c] = 'T'
+            
+            for dR,dC in directions:
+                row = r+dR
+                col = c+dC
+                dfs(row,col)
 
         for i in range(m):
             for j in range(n):
                 if board[i][j] == 'O' and ( i in [0, m-1] or j in [0, n-1] ): #boundary edges
-                    queue.append((i,j))
-                    board[i][j] = 'T'
-
-        def bfs(queue):
-            while queue:
-                r,c = queue.popleft()
-
-                for dR,dC in directions:
-                    row = r+dR
-                    col = c+dC
-
-                    if 0<=row<m and 0<=col<n and board[row][col] == 'O':
-                        queue.append((row,col))
-                        board[row][col] = 'T' #neighbor of boundary 0 so we cant capture them
-
-        bfs(queue)
+                    dfs(i,j)
 
         for i in range(m):
             for j in range(n):
