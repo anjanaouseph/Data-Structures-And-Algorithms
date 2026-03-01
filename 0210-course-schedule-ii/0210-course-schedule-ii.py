6d3cb5fef1eb27049467d89247cfe52f6903b480
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2026-02-28 18:04:42 -0700

    Time: 3 ms (76.21%), Space: 21.2 MB (24.28%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,36 +1,55 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        adj = defaultdict(list)
 
-        for a,b in prerequisites:
-            adj[a].append(b)
+# We’re given courses and prerequisite pairs.
+# This is a dependency problem because some courses must be taken before others.
+# Whenever we have items with dependencies, we can model them as a directed graph.
+# Each course is a node, and a prerequisite pair [a, b] means we draw a directed edge b → a (b must be taken before a).
+# The question asks whether we can finish all courses.
+# Since courses have prerequisites that must be taken before them, we need to find an ordering of courses that respects these dependencies. This is exactly a topological ordering. A topological ordering exists if and only if the directed graph has no cycles.
+# So the problem reduces to detecting a cycle in a directed graph.
+# DFS with visiting states, or
+# BFS using Kahn’s algorithm (topological sort).
 
-        visited = 2
-        visiting = 1
-        unvisited = 0
-        states = [unvisited]*numCourses
+        if len(prerequisites) == 0:
+            return [i for i in range(numCourses)]
 
-        order = []
+        graph = defaultdict(list) #adj list
 
-        #return True if No CYCLE, False if CYCLE
-        def dfs(node):
-            state = states[node]
-            if state == visited: return True
-            if state == visiting: 
-                return False
+        for a,b in prerequisites: #Each node maps to the nodes it has edges to.
+            graph[a].append(b)
 
-            states[node] = visiting
+        result = []
 
-            for nei in adj[node]:
-                  if not dfs(nei):
+        UNVISITED = 0
+        VISITING = 1
+        VISITED = 2
+
+        states = [0]*numCourses
+
+        def dfs(i):
+            state = states[i]
+
+            if state == VISITED:
+                return True
+            
+            if state == VISITING:
+                return False #cycle detected
+
+            states[i] = VISITING
+
+            for nei in graph[i]:
+                if not dfs(nei):
                     return False
 
-            states[node] = visited
-            order.append(node)
+            states[i] = VISITED
+            result.append(i)
+
             return True
-        
+            
+
         for i in range(numCourses):
             if not dfs(i):
                 return []
 
-        return order
+        return result      
