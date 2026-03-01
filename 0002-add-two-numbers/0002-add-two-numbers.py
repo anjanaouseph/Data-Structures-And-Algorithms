209cf8d3ab4180d1bfd4fb32f196d2dc9c942c46
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2026-02-28 21:42:01 -0700

    Time: 4 ms (45.85%), Space: 19.1 MB (88.28%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,27 +5,34 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        if not l1 and not l2:
-            return None
+        if not l1:
+            return l2
+
+        if not l2:
+            return l1
+
+        #but its said both nodes are not empty so ignore prev conditions
 
         carry = 0
-        dummy = ListNode(-1)
+        sum = 0
+
+        dummy = ListNode()
+
         curr = dummy
 
         while l1 or l2 or carry:
             v1 = l1.val if l1 else 0
             v2 = l2.val if l2 else 0
-            total_sum = v1+v2+carry
 
-            summ = total_sum%10
-            carry = total_sum//10
+            total = (v1+v2+carry)
+            sum = total%10
+            carry = total//10
 
-            result = ListNode(summ)
-
-            curr.next = result
+            Node = ListNode(sum)
+            curr.next = Node
             curr = curr.next
 
             l1 = l1.next if l1 else None
             l2 = l2.next if l2 else None
 
-        return dummy.next
+        return dummy.next        
