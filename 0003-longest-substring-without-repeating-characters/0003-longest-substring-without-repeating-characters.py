ccaebefbdbddabe07ae30c1197298bb9feec4370
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2026-02-28 19:10:28 -0700

    Time: 7 ms (96.73%), Space: 19.5 MB (29.4%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,25 +1,27 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-
         if not s:
             return 0
 
-        visited = set()
-
-        left = 0
-        right = 0
-
+        hashMap = {}
         max_len = 0
+        slow = 0 #marks the start of the current valid substring
 
-        while right < len(s):
-            while s[right] in visited:
-                visited.remove(s[left])
-                left += 1
+        for i in range(len(s)):
+            c = s[i]
+            if c in hashMap:
+                slow = max(slow, hashMap[c]+1) #if slow ptr has gone past the index of c, then take the current pos of slow ptr, meaning the duplicate is not part of current substring, else jump slow to index+1
 
-            visited.add(s[right])
+            hashMap[c] = i
 
-            max_len = max(max_len, right-left+1)
-
-            right += 1
+            max_len = max(max_len, i-slow+1)
 
         return max_len
+
+# Time Complexity : O(N)
+# Space Complexity: O(1)
+
+# If the duplicate character was seen inside the current window, we move slow past its previous index.
+# If it was seen before the current window, we do nothing and keep slow where it is.
+# This prevents slow from ever moving backward.
+        
