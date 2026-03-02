03297e8798a3802e7a3705aa80c375db2965a76d
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2026-03-01 17:43:50 -0700

    Time: 286 ms (53.98%), Space: 19.2 MB (86.2%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -2,11 +2,11 @@ class Solution:
     def longestPalindrome(self, s: str) -> str:
         if not s:
             return ""
-
+        #brute force involves looping over every substring and checking if it is a palindrome
         #palindrome can be even length and odd length, so the idea is to check for odd length and even length palindrome occurance at every index. eg bbbd bbdd
 
+        res = [0,0]
         longest = 0
-        res = ""
 
         for i in range(len(s)):
             #first lets check for odd length palindroms for every index
@@ -15,7 +15,8 @@ class Solution:
             while left >= 0 and right < len(s) and s[left] == s[right]:
                 if right-left+1 > longest:
                     longest = max(longest, right-left+1)
-                    res = s[left:right+1]
+                    res[0] = left
+                    res[1] = right #creates new string O(K) time complexity
                 left -= 1
                 right += 1
 
@@ -24,8 +25,9 @@ class Solution:
             while left >= 0 and right < len(s) and s[left] == s[right]:
                 if right-left+1 > longest:
                     longest = max(longest, right-left+1)
-                    res = s[left:right+1]
+                    res[0] = left
+                    res[1] = right
                 left -= 1
                 right += 1
 
-        return res
+        return s[res[0] : res[1]+1]
