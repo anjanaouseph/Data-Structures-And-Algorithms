8e5ba0b233fe66f926f6b0cdb745523247bdfa8c
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2026-03-08 16:26:08 -0700

    Time: 187 ms (30.98%), Space: 20.6 MB (95.02%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -1,26 +1,24 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
 
+        #she can eat from 1 till len(piles) bananas
+
         left = 1
         right = max(piles)
-        ans = -1
 
-        while left<=right:
-            mid = left+(right-left)//2
+        def eatingSpeed(mid):
+            count = 0
+            for i in range(len(piles)):
+                count += ceil(piles[i]/mid)
 
-            if self.min_integer(mid, h, piles):
-                ans = mid
+            return count
+
+        while left <= right:
+            mid = left + (right-left)//2
+
+            if eatingSpeed(mid) <= h:
                 right = mid-1
             else:
                 left = mid+1
-        return ans
 
-
-    def min_integer(self,k, h, piles):
-        count = 0
-        for i in range(len(piles)):
-            count += math.ceil(piles[i]/k)
-
-        if count<=h:
-            return True
-        return False
+        return left    
