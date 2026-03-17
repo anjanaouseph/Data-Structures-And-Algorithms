f2383786472bbfd52b593f26c3706b38a401f91d
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2026-03-17 16:10:49 -0700

    Time: 4 ms (29%), Space: 20.2 MB (16.14%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.py
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -1,46 +1,56 @@
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+
         adj = defaultdict(list)
 
         n = len(edges)
-        
-        for a, b in edges:
-            adj[a].append(b)
-            adj[b].append(a)
 
-        unvisited, visiting = 0, 1
+        for u,v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
 
-        states = [unvisited] * (n + 1)
+        VISITED = 2
+        UNVISITED = 0
+        VISITING = 1
 
-        cycle = []
+        nodes = [UNVISITED]*(len(edges)+1)
 
-        cycle_start = -1
+        result = set()
+        cycle_start = [-1]
 
-        def dfs(node, prev):   #dfs to check if cycle exists  
-            nonlocal cycle_start   
-            if states[node] == visiting:
-                cycle_start = node #add start of cycle to list
-                return True #cycle detected
+        def dfs(node, prev):
 
-            states[node] = visiting
+            state = nodes[node]
+
+            if state == VISITED:
+                return True
+
+            if state == VISITING:
+                cycle_start[0] = node
+                return False
+
+            nodes[node] = VISITING
 
             for nei in adj[node]:
                 if nei == prev:
                     continue
-                elif dfs(nei, node):
-                    if cycle_start != -1:#include only nodes that are part of the cycle.
-                        cycle.append(node)
-                    if cycle_start == node: #we came back to start of cycle again 
-                        cycle_start = -1 #reset cycle start
-                    return True
-            
-            return False
 
-        #since the graph that started as a tree from 1 to n, we do dfs for just node 1
+                elif not dfs(nei, node):
+                    if cycle_start[0] != -1:
+                        result.add(node)
+                    
+                    if cycle_start[0] == node:
+                        cycle_start[0] = -1                
+                    return False
 
-        dfs(1,-1)
+            nodes[node] = VISITED
 
-        #check if each vertex in the edges are present in the cycle, that means that edge is the culprit.
-        for i in range(n-1, -1, -1):
-            if edges[i][0] in cycle and edges[i][1] in cycle:
+            return True
+
+        dfs(1,0)
+
+        for i in range(n-1,-1,-1):
+            if edges[i][0] in result and edges[i][1] in result:
                 return edges[i]
+
+        return [-1,-1]
