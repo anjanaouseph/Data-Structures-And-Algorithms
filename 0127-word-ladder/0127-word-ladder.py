6c3a2ba2016907e415aeea419e1c60b0c3055c99
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2026-03-21 17:48:06 -0700

    Time: 55 ms (78.23%), Space: 22.3 MB (43.64%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -17,11 +17,12 @@ class Solution:
         visited = set()
         visited.add(beginWord)
 
-        #create pattern's adj list
-        for word in wordList: #O(n*m^2) for every word it will generate m*m patterns, space complexity also same O(m*m*n)
-            for j in range(len(word)):
-                pattern = word[:j]+"*"+word[j+1:]
+        #create pattern's adj list. m patterns per word, each costing O(m) to build.
+        for word in wordList: #O(n*m^2) for every word it will generate m patterns, which costO(m), space complexity also same O(m*m*n)
+            for j in range(len(word)): #m patterns
+                pattern = word[:j]+"*"+word[j+1:] #O(m)
                 neighbors[pattern].append(word)
+                
 
         
         while queue: #here nodes will be patterns and edges will be connections to the words O(V+E) v is nodes or m*m and edges will be ...
@@ -37,7 +38,58 @@ class Solution:
                     pattern = word[:j]+"*"+word[j+1:]
                     for nei in neighbors[pattern]:
                         if nei not in visited:
-                            queue.append([nei, res+1])
+                            queue.append((nei, res+1))
                             visited.add(nei)
 
+                    neighbors[pattern] = []
+
         return 0
+
+# adj list:
+# time complexity is O(m*m*n)
+# space complexity is no of unique keys. Number of keys: O(n * m).Each key costs O(m) space. Keys space = O(n * m^2)
+# Keys: O(n * m^2)
+# Values: O(n * m) so dominant term is keys.
+
+# BFS:
+# Time = O(n · m²) 
+
+# So worst case:
+# n BFS words
+# each word generates m patterns
+# each pattern list may take up to O(n)
+# That gives:
+# For each visited word:
+# generate m patterns → O(m^2)
+# scan neighbors for each of the pattern →  O(mn) each pattern can have n words
+
+# So per word:
+
+# O(m^2 + mn)
+# If BFS visits up to n words, total is:
+# Time complexity of the BFS part:
+# O(nm^2 + n^2m) = O(n^2 m)
+# Space: O(n) queue can store n words worse case
+
+# but with optmized BFS that is clearing the bucket after first scan, then
+# Full BFS after optimization
+
+# For each visited word:
+
+# generate m patterns
+# each pattern creation costs O(m)
+
+# So pattern creation total:
+
+# O(n * m^2)
+
+# Neighbor scanning total:
+
+# each bucket processed once overall
+# total stored entries across all buckets = n * m
+# so O(n * m)
+
+# Total BFS:
+
+# O(n * m^2 + n * m)
+# simplifies to O(n * m^2)
