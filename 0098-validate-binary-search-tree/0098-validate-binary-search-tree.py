2e121fe9ab2c2f3deeed80ab483bb3757d2bbfe5
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2026-04-04 14:55:19 -0700

    Time: 0 ms (100%), Space: 20.9 MB (83.31%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -7,19 +7,25 @@
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
         #here we shud check along the path
-       
-        def dfs(root, high, low):
-            if not root:
-                return True
+        #using iteration stack
+
+        stack = [(root,float('inf'),float('-inf'))]
+
+        while stack:
+            root, high, low = stack.pop()
 
             if not low < root.val < high:
                 return False
 
-            return dfs(root.left,root.val,low) and dfs(root.right,high,root.val)
-            #think in terms of upper and lower limits at each node
+            if root.left:
+                stack.append((root.left, root.val, low))
 
-        return dfs(root, float('INF'), float('-INF'))
-        return True   
+            if root.right:
+                stack.append((root.right, high, root.val))
+
+        return True
+       
+    
 
 # TC: O(N)
 # SC: O(h), which is O(n) in worst case (skewed) and O(log n) in balanced tree
