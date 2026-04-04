87658e02677e21b6d62eec0aef9f3bcbd8ef8d5b
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2026-04-04 14:43:46 -0700

    Time: 3 ms (34.8%), Space: 20.8 MB (83.31%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -4,22 +4,22 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-from collections import deque
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        if not root:
-            return False
-        
-        queue = collections.deque([(root, float("inf"), float("-inf"))])
+        #here we shud check along the path
+       
+        def dfs(root, high, low):
+            if not root:
+                return True
 
-        while queue:
-            size = len(queue)
-            for i in range(size):
-                node, max_val, min_val = queue.popleft()
-                if not min_val < node.val < max_val:
-                    return False
-                if node.left:
-                    queue.append((node.left, node.val, min_val))
-                if node.right:
-                    queue.append((node.right, max_val, node.val))
-        return True        
+            if not low < root.val < high:
+                return False
+
+            return dfs(root.left,root.val,low) and dfs(root.right,high,root.val)
+            #think in terms of upper and lower limits at each node
+
+        return dfs(root, float('INF'), float('-INF'))
+        return True   
+
+# TC: O(N)
+# SC: O(N)
