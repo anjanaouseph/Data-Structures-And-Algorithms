9f9c1a14d603d625c80cda8f185afae957ce038f
Anjana Ouseph <40512980+anjanaouseph@users.noreply.github.com>
2026-04-11 18:41:31 -0700

    Time: 378 ms (66.73%), Space: 19.4 MB (60.47%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.py
+++ 0016-3sum-closest/0016-3sum-closest.py
@@ -4,38 +4,29 @@ class Solution:
         nums.sort()
 
         closest = float('inf')
-        closest_sum = 0
+        closest_sum = nums[0]+nums[1]+nums[2]
 
-        #it says unique soln exists, there can be duplicates but i am skipping duplicates for better optimization
+        #without skipping duplicates
         for i in range(len(nums)):
-            if i != 0 and nums[i] == nums[i-1]:#t skips duplicate values for the first element of the triplet
-                continue
-
-            left = i+1
-            right = len(nums)-1
+            left, right = i+1, len(nums)-1
 
             while left < right:
-                sum = nums[left]+nums[right]+nums[i]
+                sum = nums[i]+nums[left]+nums[right]
 
-                abs_diff = abs(target-sum) #this measure the closeness
+                absolute = abs(target-sum) #measures the closeness
 
-                if abs_diff < closest:
-                    closest = min(closest, abs_diff)#stores smallest closesness so far
+                if absolute<closest:
+                    closest = min(closest, absolute)
                     closest_sum = sum
 
-                if sum >= target:#minimize the sum
+                if sum >= target:
                     right -= 1
-
-                    while (left < right and nums[right] == nums[right+1]): #skipping duplicates here
-                        right -= 1
 
                 if sum < target:
                     left += 1
 
-                    while (left<right and nums[left] == nums[left-1]):#skipping duplicates
-                        left += 1
+        return closest_sum
 
-        return closest_sum  
 
 # TC: O(nlogn)+O(n*n)
 # SC: O(1)
